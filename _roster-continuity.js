73e5f3c3353bd893fb6d300e5145710ae2e8953f
Vishaal Agartha <vishaalagartha@gmail.com>
2016-12-30 10:48:22 -0800

    parses nba win data

--- _roster-continuity.js
+++ _roster-continuity.js
@@ -1,1 +1,23 @@
-hello
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom;
+
+
+var x = d3.scaleLinear().range([0, width]),
+    y = d3.scaleLinear().range([height, 0]);
+    colors = d3.scaleOrdinal
+
+var xAxis = d3.axisBottom(x),
+    yAxis = d3.axisLeft(y);
+
+d3.csv("data.csv", type, function(error, data) {
+    if (error) throw error;
+    console.log(data);
+
+});
+
+function type(d) {
+    d.continuity = parseFloat(d.continuity);
+    return d;
+}
--- parseHTML.py
+++ parseHTML.py
@@ -1,6 +1,7 @@
-import string
+import string, csv
 
-html = open('continuity.cgi', 'r').read()
+continuity_html = open('continuity.cgi', 'r').read()
+wins_html = open('NBA_wins.html', 'r').read()
 
 teams = ["ATL", "BOS", "CHA", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NJN", "NOH", "NYK", "OKC", "ORL", "PHI", "PHO", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"]
 
@@ -10,24 +11,43 @@ for y in range (2016, 1950, -1):
     season.append(str(y)+'-'+str(y+1)[2:])
 
 
-data = []
+data = {}
 t = 0
 s = 0
-index = string.find(html, '</td>')
+index = string.find(continuity_html, '</td>')
 while index!=-1:
-    percentage = html[index-3] + html[index-2] + html[index-1]
-    if percentage[-1]!='%':
-        data.append(teams[t]+ ' ' + season[s])
-    else:
-        data.append(teams[t]+ ' ' + season[s] + ' ' + percentage)
+    percentage = continuity_html[index-3] + continuity_html[index-2] + continuity_html[index-1]
+    if percentage[-1]=='%':
+        if season[s] not in data:
+            data[season[s]] = {}
+        data[season[s]][teams[t]] = [percentage]
     t+=1
     if t==30:
         s+=1
         t=0
-    html = html[(index+2):]
-    index = string.find(html, '</td>')
+    continuity_html = continuity_html[(index+2):]
+    index = string.find(continuity_html, '</td>')
 
-csv = open('data.csv', 'w')
-csv.write('team year continuity\n')
-for d in data:
-    csv.write(d+'\n')
+season_string = 'data-stat="season"'
+index = string.find(wins_html, season_string)
+while index!=-1:
+    wins_html = wins_html[(index+len(season_string)):]
+    d = string.find(wins_html, '-')
+    season = wins_html[(d-4):(d+3)]
+    wins_html = wins_html[(d+3):]
+    if season in data: 
+        season_data = wins_html.split('\n')[0]
+        for c in range (0, len(season_data)):
+            if season_data[c].isupper():
+                team = season_data[c:c+3]
+                if team in data[season]:
+                    data[season][team].append(season_data[c+6:c+8])
+    index = string.find(wins_html, season_string)
+with open('data.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["year", "team", "continuity", "wins"])
+    for year in data:
+        for team in data[year]:
+            if len(data[year][team])==2:
+                r = [year, team, data[year][team][0], data[year][team][1]]
+                writer.writerow(r)

4ac6962fe9d7aaa7409c33272a7740665dea27bc
Vishaal Agartha <vishaalagartha@gmail.com>
2017-01-01 11:25:32 -0800

    Adds context and incomplete brush feature

--- _roster-continuity.js
+++ _roster-continuity.js
@@ -1,10 +1,9 @@
 var svg = d3.select("svg"),
-    margin = {top: 30, right: 80, bottom: 40, left: 60},
-    margin2 = {top: 430, right: 20, bottom: 30, left: 40},
+    margin = {top: 20, right: 80, bottom: 110, left: 60},
+    margin2 = {top: 430, right: 80, bottom: 30, left: 60},
     width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom;
+    height = +svg.attr("height") - margin.top - margin.bottom,
     height2 = +svg.attr("height") - margin2.top - margin2.bottom;
-
 
 var x = d3.scaleTime().range([0, width]),
     x2 = d3.scaleTime().range([0, width]),
@@ -12,14 +11,13 @@ var x = d3.scaleTime().range([0, width]),
     y2 = d3.scaleLinear().range([height2, 0]);
 
 var xAxis = d3.axisBottom(x).tickFormat(function(d){return d.getFullYear() + '-' + String(d.getFullYear()+1).slice(2,4)}),
-    xAxis2 = d3.axisBottom(x2),
+    xAxis2 = d3.axisBottom(x2).tickFormat(function(d){return d.getFullYear() + '-' + String(d.getFullYear()+1).slice(2,4)}),
     yAxis = d3.axisLeft(y).tickFormat(function(d) { return d+'%' });
 
-/*
 var brush = d3.brushX()
       .extent([[0, 0], [width, height2]])
       .on("brush end", brushed);
-*/
+
 var noise1 = d3.area()
     .curve(d3.curveMonotoneX)
     .x(function(d) { return x(d.date) })
@@ -29,8 +27,8 @@ var noise1 = d3.area()
 var noise2 = d3.area()
     .curve(d3.curveMonotoneX)
     .x(function(d) { return x2(d.date) })
-    .y0(function(d) {return y2(d.wins)-d.noise*.6 })
-    .y1(function(d) { return y2(d.wins)+d.noise*.6 });
+    .y0(function(d) {return y2(d.wins)-d.noise*.1 })
+    .y1(function(d) { return y2(d.wins)+d.noise*.1 });
 
 var wins1 = d3.line()
     .curve(d3.curveMonotoneX)
@@ -64,8 +62,11 @@ svg.append("text")
     .style("font-family", "sans-serif")
     .text("Season");
 
-var g = svg.append("g")
+var focus = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var context = svg.append("g")
+    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
 
 d3.csv("GSW_data.csv", type, function(error, data) {
@@ -78,16 +79,20 @@ d3.csv("GSW_data.csv", type, function(error, data) {
 
     x.domain(d3.extent(data, function(d) { return d.date; }));
     y.domain([0, 100]);
+    x2.domain(x.domain());
+    y2.domain(y.domain());
 
-    g.append("path")
+    focus.append("path")
+    .datum(data)
     .attr("class", "area")
     .attr("d", noise1(data));
 
-    g.append("path")
+    focus.append("path")
+    .datum(data)
     .attr("class", "line")
     .attr("d", wins1(data));
 
-    g.selectAll("circle")
+    focus.selectAll("circle")
     .data(data)
     .enter().append("circle")
     .attr( "class", "circle")
@@ -97,14 +102,33 @@ d3.csv("GSW_data.csv", type, function(error, data) {
     .on( "mouseover", handleMouseOver )
     .on( "mouseout", handleMouseOut );
 
-    g.append("g")
+    focus.append("g")
     .attr("class", "axis axis--x")
     .attr("transform", "translate(0," + height + ")")
     .call(xAxis);
 
-    g.append("g")
+    focus.append("g")
     .attr("class", "axis axis--y")
     .call(yAxis);
+
+    context.append("path")
+    .attr("class", "area")
+    .attr("d", noise2(data));
+
+    context.append("path")
+    .attr("class", "line")
+    .attr("d", wins2(data));
+
+    context.append("g")
+    .attr("class", "axis axis--x")
+    .attr("transform", "translate(0," + height2 + ")")
+    .call(xAxis2);
+
+
+    context.append("g")
+      .attr("class", "brush")
+      .call(brush)
+      .call(brush.move, x.range());
 
 });
 
@@ -123,6 +147,14 @@ function handleMouseOver(d, i) {
 
 function handleMouseOut(d, i) {
   label.style("display", "none");
+}
+
+function brushed() {
+  var s = d3.event.selection || x2.range();
+  x.domain(s.map(x2.invert, x2));
+  focus.select(".axis--x").call(xAxis);
+  focus.select(".area").attr("d", noise1);
+  //focus.select(".area").attr("d", wins1);
 }
 
 var parseDate = d3.timeParse("%Y");
